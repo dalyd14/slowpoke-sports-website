8ee49e1eb3cda6eca3250f08888831761978498e
David Daly <dalyd14@gmail.com>
2020-11-13 11:32:54 -0500

    added bootstrap, replace the navigation with bootstrap navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-function mobileNavSlide() {
-    var mobileNav = document.getElementById("mobile-navigation")
-    if (mobileNav.classList.contains("mobile-navigation-displayed")) {
-        mobileNav.classList.remove("mobile-navigation-displayed")
-    } else {
-        mobileNav.classList.add("mobile-navigation-displayed")
-    }
-}
+// function mobileNavSlide() {
+//     var mobileNav = document.getElementById("mobile-navigation")
+//     if (mobileNav.classList.contains("mobile-navigation-displayed")) {
+//         mobileNav.classList.remove("mobile-navigation-displayed")
+//     } else {
+//         mobileNav.classList.add("mobile-navigation-displayed")
+//     }
+// }
