3aaf6b988aa3f951293da140b4f5c15c5ccea820
David Daly <dalyd14@gmail.com>
2020-11-13 12:03:27 -0500

    added a tab element; added js files that hold news articles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,1 @@
-// function mobileNavSlide() {
-//     var mobileNav = document.getElementById("mobile-navigation")
-//     if (mobileNav.classList.contains("mobile-navigation-displayed")) {
-//         mobileNav.classList.remove("mobile-navigation-displayed")
-//     } else {
-//         mobileNav.classList.add("mobile-navigation-displayed")
-//     }
-// }
+console.log(nflNews)
